e914b013221cf91be8be7791e2ebd6379a0376c0
Helena Rasche <hexylena@galaxians.org>
2015-10-04 17:04:02 -0500

    Allow API route to view/add routes

--- route.go
+++ route.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -20,7 +19,7 @@ func (r *Route) IsAuthorized(cookie string) bool {
 }
 
 type RouteMappings struct {
-	Routes         []*Route
+	Routes         []Route
 	AuthCookieName string
 	Storage        string
 }
@@ -32,32 +31,54 @@ func (rm *RouteMappings) StoreToFile(path string) error {
 	}
 	defer f.Close()
 
-	jsonRoutes, err := json.Marshal(rm.Routes)
-	if err != nil {
-		return err
+	for _, route := range rm.Routes {
+		fmt.Fprintf(f, "%s\t%s\t%s\n", route.FrontendPath, route.BackendAddr, route.AuthorizedCookie)
 	}
-	f.Write(jsonRoutes)
 	return nil
 }
 
+// Init function, automatically loads from storage
 func NewRouteMapping(storage *string) *RouteMappings {
 	rm := &RouteMappings{
 		Storage: *storage,
 	}
-	rm.RestoreFromFile(storage)
+	err := rm.RestoreFromFile(storage)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%#v", rm.Routes)
 
 	return rm
 }
 
+// TODO: xml/json
 func (rm *RouteMappings) RestoreFromFile(path *string) error {
-	data, err := ioutil.ReadFile(*path)
+	// If the file doesn't exist, just return.
+	if _, err := os.Stat(*path); os.IsNotExist(err) {
+		return nil
+	}
+
+	// Open file
+	f, err := os.Open(*path)
 	if err != nil {
 		return err
 	}
-	routes := make([]*Route, 0)
+	defer f.Close()
 
-	if err := json.Unmarshal(data, routes); err != nil {
-		return err
+	routes := make([]Route, 0)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		linedata := strings.Split(scanner.Text(), "\t")
+		if len(linedata) != 3 {
+			return errors.New("Improperly formatted line! " + scanner.Text())
+		}
+		route := &Route{
+			FrontendPath:     linedata[0],
+			BackendAddr:      linedata[1],
+			AuthorizedCookie: linedata[2],
+		}
+		routes = append(routes, *route)
 	}
 
 	rm.Routes = routes
@@ -69,7 +90,7 @@ func (rm *RouteMappings) FindRoute(url string, cookie string) (*Route, error) {
 	for _, x := range rm.Routes {
 		fmt.Println(x)
 		if strings.HasPrefix(url, x.FrontendPath) && x.IsAuthorized(cookie) {
-			return x, nil
+			return &x, nil
 		}
 	}
 	return &Route{}, errors.New("Could not find route")
@@ -83,7 +104,7 @@ func (rm *RouteMappings) AddRoute(url string, backend string, cookie string) {
 		AuthorizedCookie: cookie,
 	}
 
-	rm.Routes = append(rm.Routes, r)
+	rm.Routes = append(rm.Routes, *r)
 	// After we add a route, we update the storage map
 	rm.StoreToFile(rm.Storage)
 }
