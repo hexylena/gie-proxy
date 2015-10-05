55b6c82bc7fcebfdb5670c37bf973ddc8d00652d
Helena Rasche <hexylena@galaxians.org>
2015-10-05 14:57:08 -0500

    Implement RemoveRoute, and automatically killing containers

--- route.go
+++ route.go
@@ -4,8 +4,10 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	docker "github.com/fsouza/go-dockerclient"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"strings"
 	"time"
 )
@@ -17,6 +19,7 @@ type Route struct {
 	BackendAddr      string
 	AuthorizedCookie string
 	LastSeen         time.Time
+	ContainerIds     []string `xml:ContainerIds`
 }
 
 // IsAuthorized checks if a user's cookie is valid for a given route object.
@@ -29,12 +32,17 @@ func (r *Route) Seen() {
 	r.LastSeen = time.Now()
 }
 
+func (r *Route) IsExpired(currentTime time.Time, threshold time.Duration) bool {
+	return currentTime.Sub(r.LastSeen) > threshold
+}
+
 // RouteMapping represents essentially the server state, including all
 // routes and metadata necessary to re-launch in an identical state.
 type RouteMapping struct {
-	Routes         []Route `xml:"Routes>Route"`
-	AuthCookieName string
-	Storage        string
+	Routes            []Route `xml:"Routes>Route"`
+	AuthCookieName    string
+	Storage           string
+	NoAccessThreshold time.Duration
 }
 
 // NewRouteMapping automatically loads the RouteMapping object from storage
@@ -47,7 +55,51 @@ func NewRouteMapping(storage *string) *RouteMapping {
 		panic(err)
 	}
 	fmt.Printf("%#v", rm)
+
 	return rm
+}
+
+// RemoveDeadContainers is intended to be called with the current time and a
+// threshold past which a container with no traffic should be killed. The
+// function kills that route's containers, removes the route, and saves to
+// file.
+func (rm *RouteMapping) RemoveDeadContainers(currentTime time.Time, threshold time.Duration) {
+	fmt.Printf("Removing Dead Containers %s\n", currentTime)
+	for _, route := range rm.Routes {
+		if route.IsExpired(currentTime, threshold) {
+			fmt.Printf("Found expired route %s\n", route)
+			route.KillContainers()
+			rm.RemoveRoute(&route)
+			rm.Save()
+		}
+	}
+}
+
+// KillContainers kills all containers associated with a route
+func (route *Route) KillContainers() {
+	endpoint := "unix:///var/run/docker.sock"
+	client, _ := docker.NewClient(endpoint)
+	for _, containerId := range route.ContainerIds {
+		err := client.KillContainer(docker.KillContainerOptions{
+			ID:     containerId,
+			Signal: 9,
+		})
+		if err != nil {
+			fmt.Printf("Error killing container: %s\n", err)
+		}
+	}
+}
+
+// RegisterCleaner sets up a goroutine with a ticker every N seconds which
+// checks if there are any expired containers to kill
+func (rm *RouteMapping) RegisterCleaner() {
+	// Register our new
+	ticker := time.NewTicker(time.Second * 3)
+	go func(routeMapping *RouteMapping) {
+		for t := range ticker.C {
+			rm.RemoveDeadContainers(t, rm.NoAccessThreshold)
+		}
+	}(rm)
 }
 
 // StoreToFile serializes the routemappings object to an XML file.
@@ -101,34 +153,32 @@ func (rm *RouteMapping) FindRoute(url string, cookie string) (*Route, error) {
 	return &Route{}, errors.New("Could not find route")
 }
 
+func (rm *RouteMapping) Save() {
+	rm.StoreToFile(rm.Storage)
+}
+
 // AddRoute adds a new route
-func (rm *RouteMapping) AddRoute(url string, backend string, cookie string) {
+func (rm *RouteMapping) AddRoute(url string, backend string, cookie string, containers []string) {
 	r := &Route{
 		FrontendPath:     url,
 		BackendAddr:      backend,
 		AuthorizedCookie: cookie,
 		LastSeen:         time.Now(),
+		ContainerIds:     containers,
 	}
 
-	fmt.Printf("Adding new route %#v", r)
+	fmt.Printf("Adding new route %#v\n", r)
 	rm.Routes = append(rm.Routes, *r)
 	// After we add a route, we update the storage map
-	rm.StoreToFile(rm.Storage)
+	rm.Save()
 }
 
 // RemoveRoute removes a route
 func (rm *RouteMapping) RemoveRoute(route *Route) {
-	//tmpr := &Route{
-	//FrontendPath:     url,
-	//BackendAddr:       backend,
-	//AuthorizedCookie: cookie,
-	//}
-	//for idx, x := range rm.Routes {
-	//if tmpr == x {
-	//sliceA := rm.Routes[:idx]
-	//sliceB := rm.Routes[idx+1:]
-	////rm.Routes = append(sliceA, sliceB)
-	//return
-	//}
-	//}
+	for idx, x := range rm.Routes {
+		if reflect.DeepEqual(*route, x) {
+			rm.Routes = rm.Routes[:idx+copy(rm.Routes[idx:], rm.Routes[idx+1:])]
+			return
+		}
+	}
 }
