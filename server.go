2dad9fe90b9ce17320c03c52bf11bb36260f5be0
Helena Rasche <hexylena@galaxians.org>
2016-02-25 16:45:03 +0000

    More useful comment on api route

--- server.go
+++ server.go
@@ -27,13 +27,16 @@ func (f *frontend) Start(rm *RouteMapping) {
 		Frontend:     f,
 	}
 
+	// The API route only handles requests with an absolute path of /api. Given
+	// that the frontend.Path is usually set, this means it will be completely
+	// unavailable to external access, and only available from localhost. This
+	// is a win for security, as only Galaxy should be talking to the API
+	mux.Handle("/api", apiHandler)
 	// The slash route handles ALL requests by passing to the request_handler
 	// object
-	mux.Handle("/api", apiHandler)
 	mux.Handle("/", requestHandler)
 	// Here we then launch the server from mux
 	srv := &http.Server{Handler: mux, Addr: f.Addr}
-
 	// Start
 	log.Info("Listening on %s %s", f.Addr, f.Path)
 	if err := srv.ListenAndServe(); err != nil {
