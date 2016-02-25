2078ebfc0554115af7a2cae3943696ba42cb4022
Helena Rasche <hexylena@galaxians.org>
2016-02-25 00:16:58 -0600

    Switch to codegansta/cli

--- main.go
+++ main.go
@@ -9,15 +9,9 @@ import (
 	"os"
 	"strings"
 	"time"
+	"github.com/codegangsta/cli"
 )
 
-var addr = flag.String("listen", "0.0.0.0:8080", "address to listen on")
-var path = flag.String("listen_path", "/galaxy/gie_proxy", "path to listen on (for cookies)")
-var cookieName = flag.String("cookie_name", "galaxysession", "cookie name")
-var sessionMap = flag.String("storage", "./sessionMap.xml", "Session map file. Used to (re)store route lists across restarts")
-var apiKey = flag.String("api_key", "THE_DEFAULT_IS_NOT_SECURE", "Key to access the API")
-var noAccessThreshold = flag.Int("noaccess", 60, "Length of time a proxy route must be unused before automatically being removed")
-var dockerEndpoint = flag.String("docker", "unix:///var/run/docker.sock", "Endpoint at which we can access docker. No TLS Support yet")
 var log = logging.MustGetLogger("main")
 
 type frontend struct {
@@ -105,7 +99,55 @@ func connectRoute(h *requestHandler, w http.ResponseWriter, r *http.Request) err
 	return err
 }
 
-func main() {
+func main(){
+	app := cli.NewApp()
+	app.Name = "gie-proxy"
+	app.Usage = "proxy for Galaxy GIEs"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "listenAddr",
+            Value: "0.0.0.0:8800",
+			Usage: "address to listen on",
+		},
+		cli.StringFlag{
+			Name:  "listenPath",
+			Value: "/galaxy/gie_proxy",
+			Usage: "path to listen on (for cookies)",
+		},
+		cli.StringFlag{
+			Name:  "cookieName",
+			Usage: "cookie name",
+			Value: "galaxysession",
+		},
+        cli.StringFlag{
+            Name: "storage",
+            Value: "./sessionMap.xml",
+            Usage: "Session map file. Used to (re)store route lists across restarts",
+        },
+        cli.StringFlag{
+            Name: "apiKey",
+            Value: "THE_DEFAULT_IS_NOT_SECURE",
+            Usage: "Key to access to the API",
+        },
+        cli.IntFlag{
+            Name: "noAccess",
+            Value: 60,
+            Usage: "Length of time a proxy route must be unused before automatically being removed",
+        },
+        cli.StringFlag{
+            Name: "dockerAddr",
+            Value: "unix:///var/run/docker.sock",
+            Usage: "Endpoint at which we can access docker. No TLS Support yet",
+        },
+	}
+
+	app.Action = func(c *cli.Context) {
+        //c.Int("buffSize"),
+	}
+	app.Run(os.Args)
+}
+
+func main2(sessionMap, dockerEndpoint, cookieName, listenAddr, listenPath string, noAccessThreshold int) {
 	// Logging
 	format := logging.MustStringFormatter(
 		"%{color}%{time:15:04:05.000} %{shortfunc} > %{level:.4s} %{id:03x}%{color:reset} %{message}",
@@ -117,18 +159,16 @@ func main() {
 	log.SetBackend(backend1Leveled)
 
 	log.Debug("Starting up")
-	// Cli Args
-	flag.Parse()
 	// Load up route mapping
 	rm := NewRouteMapping(sessionMap, dockerEndpoint)
-	rm.AuthCookieName = *cookieName
-	rm.NoAccessThreshold = time.Second * time.Duration(*noAccessThreshold)
+	rm.AuthCookieName = cookieName
+	rm.NoAccessThreshold = time.Second * time.Duration(noAccessThreshold)
 	rm.Save()
 	log.Debug("Loaded RouteMapping from Storage %s", rm)
 	// Build the frontend
 	f := &frontend{
-		Addr: *addr,
-		Path: *path,
+		Addr: listenAddr,
+		Path: listenPath,
 	}
 	// Start our proxy
 	log.Info("Starting frontend ...")
