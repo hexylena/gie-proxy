5bbc473f943b6e18b8e6fb751516b2570358177e
Helena Rasche <hexylena@galaxians.org>
2016-02-25 16:37:34 +0000

    More configurable clean interval

--- types.go
+++ types.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	docker "github.com/fsouza/go-dockerclient"
 	"net/http"
+	"time"
 )
 
 type frontend struct {
@@ -20,4 +22,27 @@ type apiHandler struct {
 	Transport    *http.Transport
 	RouteMapping *RouteMapping
 	APIKey       string
+}
+
+// Route represents connection information to wire up a frontend request to a
+// backend
+type Route struct {
+	FrontendPath     string
+	BackendAddr      string
+	AuthorizedCookie string
+	LastSeen         time.Time
+	ContainerIds     []string `xml:"ContainerIds"`
+	Expired          bool
+}
+
+// RouteMapping represents essentially the server state, including all
+// routes and metadata necessary to re-launch in an identical state.
+type RouteMapping struct {
+	Routes            []Route `xml:"Routes>Route"`
+	AuthCookieName    string
+	Storage           string
+	NoAccessThreshold time.Duration
+	DockerEndpoint    string
+	client            *docker.Client
+	CleanInterval     time.Duration
 }
