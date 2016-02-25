4a2dc2e718c4e598f2f5d6c8b34ff408e6949976
Helena Rasche <hexylena@galaxians.org>
2016-02-25 00:29:20 -0600

    Major refactor

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (f *frontend) Start(rm *RouteMapping, apiKey string, cookieName string) {
+	mux := http.NewServeMux()
+
+	// Main request handler, processes every incoming request
+	var requestHandler http.Handler = &requestHandler{
+		Transport: &http.Transport{
+			DisableKeepAlives:  false,
+			DisableCompression: false,
+		},
+		RouteMapping: rm,
+		Frontend:     f,
+		CookieName:   cookieName,
+	}
+	rm.RegisterCleaner()
+
+	var apiHandler http.Handler = &apiHandler{
+		Transport: &http.Transport{
+			DisableKeepAlives:  false,
+			DisableCompression: false,
+		},
+		RouteMapping: rm,
+		ApiKey:       apiKey,
+	}
+
+	// The slash route handles ALL requests by passing to the request_handler
+	// object
+	mux.Handle("/api", apiHandler)
+	mux.Handle("/", requestHandler)
+	// Here we then launch the server from mux
+	srv := &http.Server{Handler: mux, Addr: f.Addr}
+
+	// Start
+	if err := srv.ListenAndServe(); err != nil {
+		log.Critical("Starting frontend failed: %v", err)
+	}
+}
---
+++ types.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+type frontend struct {
+	Addr string
+	Path string
+}
+
+type requestHandler struct {
+	Transport    *http.Transport
+	RouteMapping *RouteMapping
+	Frontend     *frontend
+	CookieName   string
+}
+
+type apiHandler struct {
+	Transport    *http.Transport
+	RouteMapping *RouteMapping
+	ApiKey       string
+}
